069a3083a91ad58f992f8f91f60e87d5af6a42fd
Justice <asu23justice@gmail.com>
2026-03-25 10:39:37 -0400

    types

---
+++ src/content/types.ts
@@ -0,0 +1,14 @@
+export type Project = {
+  slug: string;
+  name: string;
+  summary: string;
+  impact: string;
+  stack: string[];
+  repo?: string;
+  caseStudy?: string;
+  overview?: string;
+  myRole?: string;
+  problem?: string;
+  approach?: string;
+  outcome?: string;
+};
